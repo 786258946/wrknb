bdfc639eed50f90c085b00238565bc3789caba3d
王瑞凯 <786258946@qq.com>
2021-02-05 16:47:33 +0800

    Initial Commit

---
+++ siteinfo.js
@@ -0,0 +1,10 @@
+﻿module.exports = {
+    name: "蜂巢小屋",
+    uniacid: "57",
+    acid: "57",
+    multiid: "0",
+    version: "3.1.49",
+  siteroot:  "https://w71.saifer.club/app/index.php",
+  apiroot: "https://w71.saifer.club/web/index.php?store_id=2&r=api/",
+    design_method: "3"
+};
